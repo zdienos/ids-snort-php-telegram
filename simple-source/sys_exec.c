21b546fb2fbb1b80eb2e2226c3e2c4f7621a0633
zdienos <zdienos@yahoo.com>
2024-02-24 09:57:05 +0800

    source

---
+++ simple-source/sys_exec.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdlib.h>
+#include <mysql.h>
+
+my_bool sys_exec_init(
+  UDF_INIT *initid
+, UDF_ARGS *args
+, char *message
+){
+  unsigned int i=0;
+  if(args->arg_count == 1
+  && args->arg_type[i]==STRING_RESULT){
+    return 0;
+  } else {
+    strcpy(
+      message
+    , "Expected exactly one string type parameter"
+    );
+    return 1;
+  }
+}
+void sys_exec_deinit(
+  UDF_INIT *initid
+){
+}
+my_ulonglong sys_exec(
+  UDF_INIT *initid
+, UDF_ARGS *args
+, char *is_null
+, char *error
+){
+  return system(args->args[0]);
+}
