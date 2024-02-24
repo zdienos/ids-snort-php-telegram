c40f152a3cceeecdbb2a4672d07a5640b92602c9
zdienos <zdienos@yahoo.com>
2024-02-24 10:22:05 +0800

    udf

--- snort.php
+++ snort.php
@@ -2,8 +2,7 @@
 
 // $telegramchatid = "xsssxx"; 
 // $telegrambot = "yyy:xsssxx";
-$telegramchatid = "698990214"; //dino
-$telegrambot = "6705155258:AAE3ZW2wH6o13wMYk99-lrJbBaePKDJE-A4";
+
 
 
 function telegram($msg)
